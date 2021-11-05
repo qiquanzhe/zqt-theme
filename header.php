29efd92883d34dff07c3702667c03f06b310b99f
qiquanzhe <sunnydays2020@126.com>
2021-11-05 15:25:24 +0800

    site description

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="Description" content="<?php bloginfo('description'); ?>" />
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 	<!-- Bootstrap Core CSS -->
 	<!-- https://bootswatch.com/superhero/ -->
