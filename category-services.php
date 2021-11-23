319f02b66561462d5744d717ac5eba37d00722a1
qiquanzhe <sunnydays2020@126.com>
2021-11-23 15:54:38 +0800

    update services

--- category-services.php
+++ category-services.php
@@ -2,21 +2,19 @@
 
 /**
  * 
- * 分类目录存档模板文件。
+ * 分类[业务范围]目录存档模板文件。
  *  
  */
 
 get_header(); ?>
+
 <div class="box">
     <div class="banner">
         <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/img/business_banner.png" alt="banner">
     </div>
     <div class="container">
-		<div class="breadcrumbs text-end">
-			<?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?>
-			</div>
         <div class="cat-title my-md-3">
-			<div>
+            <div>
                 <?php single_cat_title(); ?>
             </div>
         </div>
@@ -35,109 +33,46 @@ get_header(); ?>
                 }
                 ?>
             </ul>
-            <div class="col-md-9 my-md-4" id="accordion">
+            <div class="col-md-9">
                 <?php
                 if ($cat == 2) {
-                    $tag = get_term_by('slug', 'top-services', 'post_tag');
-                    if ($tag) :
-                        query_posts('orderby=date&order=ASC&tag_id=' . $tag->term_id);
-                        while (have_posts()) :
-                            the_post(); ?>
-                            <div class="card services-card">
-                                <div class="card-header services-header">
-                                    <a class="btn" data-bs-toggle="collapse" href="#collapse<?php the_ID(); ?>">
-                                        <?php the_title(); ?>
-                                    </a>
-                                </div>
-                                <div id="collapse<?php the_ID(); ?>" class="collapse" data-bs-parent="#accordion">
-                                    <div class="card-body">
-                                        <?php the_content(); ?>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php
-                        endwhile;
-                    endif;
+                    $query_post_args_new = array(
+                        'showposts' => $limit = (20 - $post_num),
+                        'cat' => $cat,
+                        "paged" => $paged,
+                        'orderby' => 'ID',
+                        'post__in' => get_option('sticky_posts'),
+                        'order' => 'ASC'
+                    );
                 } else {
-                    query_posts('orderby=date&order=ASC&cat=' . $cat);
-                    while (have_posts()) :
-                        the_post(); ?>
-                        <div class="card services-card">
-                            <div class="card-header services-header">
-                                <a class="btn" data-bs-toggle="collapse" href="#collapse<?php the_ID(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </div>
-                            <div id="collapse<?php the_ID(); ?>" class="collapse" data-bs-parent="#accordion">
-                                <div class="card-body">
-                                    <?php the_content(); ?>
-                                </div>
-                            </div>
-                        </div>
-                <?php
-                    endwhile;
+                    $query_post_args_new = array(
+                        'showposts' => $limit = (20 - $post_num),
+                        'cat' => $cat,
+                        "paged" => $paged,
+                        'orderby' => 'ID',
+                        'order' => 'ASC'
+                    );
                 }
+                    $var_posts = new WP_Query($query_post_args_new);
                 ?>
-                <!-- <div class="card">
-                    <div class="card-header">
-
-                    </div>
-                </div> -->
-            </div>
-            <div class="col-md-9 my-md-3 text-center exampled-div">
-                <?php
-                if ($cat == 2) {
-                    $tag = get_term_by('slug', 'top-services', 'post_tag');
-                    if ($tag) {
-                        query_posts('showposts=-1&tag_id=' . $tag->term_id);
-                        while (have_posts()) :
-                            the_post(); ?>
-                            <div class="row">
-                                <a href="<?php the_permalink() ?>" title="<?php the_title() ?>" class="col-md-8">
-                                    <span class="mx-md-4 col-md-4 date-span"><?php the_time('Y-n-d'); ?></span>
-                                    <span class="col-md-8 title-span"><?php the_title(); ?></span>
-                                </a>
-                                <span class="col-md-3">
-                                    <?php
-                                    $tags = wp_get_post_tags($post->ID);
-                                    foreach ($tags as $tag) {
-                                        echo '<span class="badge rounded-pill bg-warning">' . $tag->name . '</span>';
-                                    }
-                                    ?>
-                                </span>
-                            </div>
+                    <ul class="news-list">
                         <?php
-                        // the_title();
-                        endwhile;
-                    }
-                } else {
-                    query_posts('posts_per_page=10&cat=' . $cat);
-                    while (have_posts()) :
-                        the_post();
+                        while ($var_posts->have_posts()) {
+                            $var_posts->the_post();
                         ?>
-                        <div class="row">
-                            <a href="<?php the_permalink() ?>" title="<?php the_title() ?>" class="col-md-8">
-                                <span class="col-md-4 mx-md-4 date-span"><?php the_time('Y-n-d'); ?></span>
-                                <span class="title-span col-md-8"><?php the_title(); ?></span>
-                            </a>
-                            <span class="col-md-3">
-                                <?php
-                                $tags = wp_get_post_tags($post->ID);
-                                foreach ($tags as $tag) {
-                                    echo '<span class="badge rounded-pill bg-warning">' . $tag->name . '</span>';
-                                }
-                                ?>
-                            </span>
-                        </div>
-                <?php
-                    endwhile;
-                }
-                ?>
+                            <li class="row">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="col-md-10">
+                                    <?php the_title(); ?></a>
+                            </li>
+                    <?php
+                        }
+                    wp_reset_query();
+                    wp_pagenavi();
+                    ?>
+                    </ul>
             </div>
         </div>
     </div>
-</div>
-<div>
 </div>
 <?php
 get_footer();
--- page-example.php
+++ page-example.php
@@ -9,9 +9,7 @@ get_header();?>
         <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/img/banner-example.png" alt="banner">
     </div>
     <div class="container">
-		<div class="breadcrumbs text-end">
-			<?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?>
-			</div>
+		
     <?php the_content(); ?>
     </div>
 </div>
